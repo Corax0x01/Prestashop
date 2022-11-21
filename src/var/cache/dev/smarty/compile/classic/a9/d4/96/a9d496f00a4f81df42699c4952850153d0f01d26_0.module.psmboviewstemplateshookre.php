28f3a5ebc61f3f90d770d17b87642de194443336
Kuba <szykuba@gmail.com>
2022-11-21 12:39:30 +0100

    product import

--- src/var/cache/dev/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
+++ src/var/cache/dev/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.43, created on 2022-11-20 20:06:17
+/* Smarty version 3.1.43, created on 2022-11-21 12:16:12
   from 'module:psmboviewstemplateshookre' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.43',
-  'unifunc' => 'content_637a7aa969b090_49570987',
+  'unifunc' => 'content_637b5dfc919176_50216301',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a9d496f00a4f81df42699c4952850153d0f01d26' => 
     array (
       0 => 'module:psmboviewstemplateshookre',
-      1 => 1668971070,
+      1 => 1668971801,
       2 => 'module',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_637a7aa969b090_49570987 (Smarty_Internal_Template $_smarty_tpl) {
+function content_637b5dfc919176_50216301 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- begin /var/www/html/modules/ps_mbo/views/templates/hook/recommended-modules.tpl -->
 <?php echo '<script'; ?>
 >
