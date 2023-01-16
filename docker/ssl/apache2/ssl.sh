de6369b3d394565f8894347f37f4bb2645b168be
Kuba <szykuba@gmail.com>
2023-01-16 13:21:47 +0100

    Update ssl.sh

--- docker/ssl/apache2/ssl.sh
+++ docker/ssl/apache2/ssl.sh
@@ -4,13 +4,9 @@ openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout /etc/ssl/privat
 
 rm -rf /etc/apache2/sites-available/000-default.conf
 
-
 cp /docker/000-default.conf /etc/apache2/sites-available/000-default.conf
-
 
 a2enmod ssl
 
-
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
-service apache2 reload
-
+apachectl -D FOREGROUND
