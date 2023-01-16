87023f18fde45f349a32cf528eb1864e14b3a3cd
Kuba <szykuba@gmail.com>
2023-01-16 14:30:35 +0100

    Update ssl.sh

--- docker/ssl/apache2/ssl.sh
+++ docker/ssl/apache2/ssl.sh
@@ -9,5 +9,3 @@ cp /docker/000-default.conf /etc/apache2/sites-available/000-default.conf
 a2enmod ssl
 
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
-service apache2 restart &
-#exec tail -f /dev/null
